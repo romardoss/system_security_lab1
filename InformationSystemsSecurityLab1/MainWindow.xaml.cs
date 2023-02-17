591bff35d51e04bd7eb5750a8c04c97c59a6b4b6
romardoss <romassuper@gmail.com>
2023-02-17 19:18:22 +0200

    UI design updates

--- InformationSystemsSecurityLab1/MainWindow.xaml.cs
+++ InformationSystemsSecurityLab1/MainWindow.xaml.cs
@@ -1,28 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace InformationSystemsSecurityLab1
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OpenFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog
+            {
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.CommonDocuments)
+            };
+            if (openFile.ShowDialog() == true ) {
+                WorkingText.Document.Blocks.Clear();
+                WorkingText.AppendText(File.ReadAllText(openFile.FileName));
+            }
+        }
+
+        private void NewFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            WorkingText.Document.Blocks.Clear();
+        }
+
+        private string StringFromRichTextBox(RichTextBox richTextBox)
+        {
+            TextRange textRange = new TextRange(
+                richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd);
+            return textRange.Text;
         }
     }
 }
