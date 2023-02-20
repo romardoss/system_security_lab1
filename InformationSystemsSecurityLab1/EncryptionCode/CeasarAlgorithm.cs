1b50ed371b4923e82dcdc488f88a3189dbbc518c
romardoss <romassuper@gmail.com>
2023-02-20 20:01:54 +0200

    encryption and decryption added

---
+++ InformationSystemsSecurityLab1/EncryptionCode/CeasarAlgorithm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InformationSystemsSecurityLab1.EncryptionCode
+{
+    public class CeasarAlgorithm
+    {
+        public static string Encrypt(string input, int key)
+        {
+            char charKey = (char)key;
+            char[] letters = input.ToCharArray();
+            for(int i = 0; i < letters.Length; i++)
+            {
+                letters[i] += charKey;
+            }
+            return new string(letters);
+        }
+
+        public static string Decrypt(string input, int key)
+        {
+            char charKey = (char)key;
+            char[] letters = input.ToCharArray();
+            for (int i = 0; i < letters.Length; i++)
+            {
+                letters[i] -= charKey;
+            }
+            return new string(letters);
+        }
+    }
+}
--- InformationSystemsSecurityLab1/MainWindow.xaml.cs
+++ InformationSystemsSecurityLab1/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Win32;
+﻿using InformationSystemsSecurityLab1.EncryptionCode;
+using Microsoft.Win32;
 using System;
 using System.IO;
 using System.Windows;
@@ -36,6 +37,49 @@ namespace InformationSystemsSecurityLab1
             TextRange textRange = new TextRange(
                 richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd);
             return textRange.Text;
+        }
+
+        private void EncryptButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int key = GetKey(KeyText.Text);
+                string input = StringFromRichTextBox(WorkingText);
+                string answer = CeasarAlgorithm.Encrypt(input, key);
+                PrintAnswer(answer);
+            }
+            catch (Exception) { }
+        }
+
+        private void DecryptButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int key = GetKey(KeyText.Text);
+                string input = StringFromRichTextBox(WorkingText);
+                string answer = CeasarAlgorithm.Decrypt(input, key);
+                PrintAnswer(answer);
+            }
+            catch (Exception) { }
+        }
+
+        private void PrintAnswer(string answer)
+        {
+            WorkingText.Document.Blocks.Clear();
+            WorkingText.AppendText(answer);
+        }
+
+        private int GetKey(string key)
+        {
+            if (int.TryParse(key, out int answer))
+            {
+                return answer;
+            }
+            else
+            {
+                MessageBox.Show("Key must be a number");
+                throw new Exception();
+            }
         }
     }
 }
